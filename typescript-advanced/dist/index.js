70c1a07fe24c9d16b05443baf986d351126e0d82
iamsukanta <sukantabalacste2016@gmail.com>
2025-10-11 21:51:22 +0200

    in operator in typescript

--- typescript-advanced/dist/index.js
+++ typescript-advanced/dist/index.js
@@ -1,7 +1,12 @@
 "use strict";
 const myId = 'id';
 console.log(myId);
-const person = 'John Doe';
+const person = { id: 1, name: 'John' };
 const personType = typeof person;
 console.log(personType);
+const optionalUser = {
+    email: 'sampleuser@gmail.com',
+    name: 'Alice'
+};
+console.log(optionalUser);
 //# sourceMappingURL=index.js.map
--- typescript-advanced/index.ts
+++ typescript-advanced/index.ts
@@ -11,3 +11,16 @@ console.log(myId);
 const person = { id: 1, name: 'John' };
 const personType = typeof person;
 console.log(personType); // "object"
+
+
+
+type OptionalUser = {
+    [K in keyof User]?: User[K]
+}
+
+const optionalUser: OptionalUser = {
+    email: 'sampleuser@gmail.com',
+    name: 'Alice'
+}
+
+console.log(optionalUser);
