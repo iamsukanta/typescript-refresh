c9092883eea5b30dab319ae772d438849dc3b230
iamsukanta <sukantabalacste2016@gmail.com>
2025-10-22 23:10:15 +0200

    Closure concept added

--- typescript-advanced/app.js
+++ typescript-advanced/app.js
@@ -1,13 +1,22 @@
 // Scope practice in JavaScript
-function outer() {
-  
+// function myFunction() {
+//   var name = "Sukanta";
+// }
+// myFunction();
 
-  function inner() {
-    console.log(outerVar); // ✅ Accesses outer variable
+// console.log(i); // This will throw a ReferenceError because 'name' is not defined in this scope.
+// var i;
+
+// Closere practice in JavaScript
+
+function closureTest() {
+  let count = 0;
+  return function increment() {
+    count++;
+    console.log(count);
+    return count;
   }
-
-  inner();
-  var outerVar = "I am outer";
 }
 
-outer();
+const incrementFunction = closureTest();
+console.log(incrementFunction());
