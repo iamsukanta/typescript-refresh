08708439f79f8289ec829a11d4e766d683c7a834
iamsukanta <sukantabalacste2016@gmail.com>
2025-10-23 23:55:34 +0200

    new div added

--- random-object-browser-game/main.js
+++ random-object-browser-game/main.js
@@ -1,4 +1,6 @@
 let body = document.getElementById("body");
+let scoreBoard = document.createElement("p");
+scoreBoard.innerText = "Score: 0";
 let element = document.createElement("div");
 let score = 0;
 element.innerText = "Hello";
@@ -6,13 +8,17 @@ element.style.position = "absolute";
 element.style.left = "50%";
 element.style.top = "50%";
 body.appendChild(element);
+body.appendChild(scoreBoard);
 
 setInterval(() => {
     element.style.left = Math.random() * window.innerWidth + "px";
     element.style.top = Math.random() * window.innerHeight + "px";
 }, 2000);
 
+// moving object in a definite area not whole screen
+setInterval(() => {
+
 element.addEventListener("click", () => {
     score++;
-    console.log("Score: " + score);
-})
+    scoreBoard.innerText = "Score: " + score;
+});
