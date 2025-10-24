0346b7db1cedd0af7cdb7b4ade45e39eeec9b1ec
iamsukanta <sukantabalacste2016@gmail.com>
2025-10-24 23:15:03 +0200

    Play, pause, reset button added

--- typescript-advanced/app.js
+++ typescript-advanced/app.js
@@ -1,22 +1,59 @@
-// Scope practice in JavaScript
-// function myFunction() {
-//   var name = "Sukanta";
+// const promise1 = new Promise((resolve, reject) => {
+//   return fetch('https://jsonplaceholder.typicode.com/posts?limit=5')
+//     .then(response => response.json())
+//     .then(data => resolve(data))
+//     .catch(error => reject(error));
+// });
+
+// const promise2 = new Promise((resolve, reject) => {
+//   return fetch('https://klljsonplaceholder.typicode.com/comments?limit=5')
+//     .then(response => response.json())
+//     .then(data => resolve(data))
+//     .catch(error => reject(error));
+// })
+
+
+// Promise.allSettled([promise1, promise2])
+//   .then((result) => {
+//     console.log('Posts:', result);
+//     // console.log('Comments:', comments);
+//   })
+//   .catch(error => {
+//     console.error('Error fetching data:', error);
+//   }) 
+  
+  
+// const myPromise = new Promise((resolve, reject) => {
+//   resolve('Promise resolved successfully!');
+// });
+
+// function myPromiseHandler(value) {
+//   return new Promise((resolve, reject) => {
+//     if(value > 10) {
+//       resolve('Value is greater than 10');
+//     } else {
+//       reject('Value is less than or equal to 10');
+//     }
+//   });
 // }
-// myFunction();
 
-// console.log(i); // This will throw a ReferenceError because 'name' is not defined in this scope.
-// var i;
+// myPromiseHandler(9)
+//   .then((message)=> {
+//     console.log(message);
+//   })
+//   .catch((error) => {
+//     console.error(error);    
+//   })
 
-// Closere practice in JavaScript
 
-function closureTest() {
-  let count = 0;
-  return function increment() {
-    count++;
-    console.log(count);
-    return count;
-  }
+async function testAsysnc() {
+  return 1010;
 }
 
-const incrementFunction = closureTest();
-console.log(incrementFunction());
+
+async function wrapAsync() {
+  const result  = await testAsysnc();
+  console.log(result);
+}
+
+console.log(wrapAsync());
